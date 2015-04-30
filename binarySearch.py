eab14b3fa3a21ea80a0a5792134488494899dd1b
Theodore Randolph <trandolp@gmail.com>
2015-04-30 15:15:27 -0700

    Makes more sense now. I got a complaint that my binarySearchWithDuplicates reverted to linear search. Well, now it’s only using binarySearch.

--- binarySearch.py
+++ binarySearch.py
@@ -1,3 +1,4 @@
+
 def binarySearch(arr, target):
     if len(arr) == 0: return -1
     first, last = 0, len(arr)
@@ -9,20 +10,14 @@ def binarySearch(arr, target):
     return -1 if result == -1 else mid + 1 + result
 
 def binarySearchWithDuplicates(arr, target):
-    randomone = binarySearch(arr,target)
-    if randomone > 0:
-        lastone = randomone
-        firstone = randomone
-        randomone = binarySearch(arr[:randomone], target)
-        while randomone > -1:
-            firstone = randomone
-            randomone = binarySearch(arr[:randomone], target)
-        randomone = binarySearch(arr[lastone+1:], target)
-        while randomone > -1:
-            lastone += randomone + 1
-            randomone = binarySearch(arr[lastone+1:], target)
-        return firstone, lastone
-    return (-1, -1)
+    randomone = binarySearch(arr, target)
+    if randomone < 0 : return -1, -1
+    search = lambda array: binarySearch(array, target)
+    loop1 = lambda array, searchfirst, first: first if searchfirst == -1 else loop1(array[:searchfirst], search(array[:searchfirst]),searchfirst)
+    loop2 = lambda array, searchlast, last: last if searchlast == -1 else loop2(array[searchlast+1:], search(array[searchlast+1:]),last + searchlast + 1)
+    first = loop1(arr[:randomone],search(arr[:randomone]),randomone)
+    last = loop2(arr[randomone+1:],search(arr[randomone+1:]),randomone)
+    return first, last
     
 for i in range(0, 10):
     print(binarySearch([1,2,3,4,5,6], i))
